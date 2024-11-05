e19b465f9d2c942c01dff3727664c4675a746772
coni24 <169170065+coni24@users.noreply.github.com>
2024-11-04 21:27:31 -0300

    2

--- Concierto/ConciertoApp/forms.py
+++ Concierto/ConciertoApp/forms.py
@@ -4,14 +4,26 @@ from ConciertoApp.models import Entrada, Persona, Concierto
 class FormEntrada(forms.ModelForm):
     class Meta:
         model = Entrada
-        fields = '__all__'
+        fields = ['concierto', 'persona', 'Categoria', 'Descripcion', 'NumeroAsiento', 'Precio', 'Sector']
 
 class FormPersona(forms.ModelForm):
     class Meta:
         model = Persona
-        fields = '__all__'
+        fields = ['RUT', 'Nombre', 'Apellido', 'Telefono', 'Edad', 'Correo']
+
+    def clean_Nombre(self):
+        nombre = self.cleaned_data['Nombre']
+        if len(nombre) > 20:
+            raise forms.ValidationError("El largo maximo del nombre son 20 caracteres")
+        return nombre
+    
+    def clean_Correo(self):
+        email = self.cleaned_data['Correo']
+        if email.find('@') == -1:
+            raise forms.ValidationError("El correo debe contener @")
+        return email
 
 class FormConcierto(forms.ModelForm):
     class Meta:
         model = Concierto
-        fields = '__all__'
+        fields = ['Fecha', 'Hora', 'Lugar', 'Categoria', 'Capacidad']
